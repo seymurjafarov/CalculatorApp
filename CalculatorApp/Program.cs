57a4aef849daacb00461d048642356b2d32ecba1
seymurjafarov <jafar.seymur@gmail.com>
2024-04-16 22:07:29 +0400

    Project initially added

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter second number");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter operation + , - , * , /");
+            char oper = (char)Console.Read();
+
+            double result = 0;
+
+            switch (oper)
+            {
+                case '+': result = a + b; break;
+                case '-': result = a - b; break;
+                case '*': result = a * b; break;
+                case '/': result = a / b; break;
+                default: Console.WriteLine("not found"); break;
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
